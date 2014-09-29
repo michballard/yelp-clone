5391eabe54c240262fd8b780b72a58b57f0fe93a
michballard <michballard@gmail.com>
2014-09-29 12:04:14 +0100

    Created the shell of project using Rails, added postgress database, set up restaurant as a class, added tests (no restaurants added, add a restaurant) and passed tests

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'restaurants' do 
+	context 'no restaurants have been added' do
+		it 'should display a prompt to add a restaurant' do 
+			visit '/restaurants'
+			expect(page).to have_content 'No restaurants yet'
+			expect(page).to have_link 'Add a restaurant'
+		end
+	end
+	context 'restaurants have been added' do 
+		before do
+			Restaurant.create(name: 'KFC')
+		end
+		it "should display them" do 
+			visit '/restaurants'
+			expect(page).to have_content('KFC')
+			expect(page).not_to have_content("No restaurants")
+		end
+	end
+end
