23fe9aa4a07358dd37c21ebb8cd5d923b83b80a6
michballard <michballard@gmail.com>
2014-10-01 11:59:28 +0100

    Average rating number replaced with stars.  Tests added and passed

---
+++ spec/helpers/reviews_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe ReviewsHelper, :type => :helper do 
+  context '#star_rating' do 
+    it 'does nothing for not a number' do 
+      expect(helper.star_rating('N/A')).to eq 'N/A'
+    end
+
+    it 'returns 5 black stars for 5 rating' do 
+      expect(helper.star_rating(5)).to eq '★★★★★'
+    end
+
+    it 'returns 3 black stars and 2 white stars for rating of 3' do 
+    	expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+  end
+end
